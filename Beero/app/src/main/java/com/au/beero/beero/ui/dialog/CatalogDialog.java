6cfc97de2056af9d10a08e1fe76ece084099bdb6
Thức Phan <shinta.bmt@gmail.com>
2015-09-16 23:58:07 +0700

    handle dialog

--- Beero/app/src/main/java/com/au/beero/beero/ui/dialog/CatalogDialog.java
+++ Beero/app/src/main/java/com/au/beero/beero/ui/dialog/CatalogDialog.java
@@ -1,6 +1,7 @@
 package com.au.beero.beero.ui.dialog;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -79,9 +80,46 @@ public class CatalogDialog extends CustomDialog implements View.OnClickListener 
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 return false;
             }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                handleProgressBar(true);
+                System.out.println("onPageStarted");
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                handleProgressBar(false);
+                System.out.println("onPageFinished");
+            }
+
+//            @Override
+//            public void onLoadResource(WebView view, String url) {
+//                super.onLoadResource(view, url);
+//                handleProgressBar(true);
+//            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                showToast(description);
+                handleProgressBar(false);
+            }
         });
         mWebview.setWebChromeClient(new WebChromeClient() {
-
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+            }
         });
+    }
+    private void handleProgressBar(boolean isShow){
+        if (isShow){
+            mProgressBar.setVisibility(View.VISIBLE);
+        } else {
+            mProgressBar.setVisibility(View.GONE);
+        }
     }
 }
